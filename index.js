3d409ae407495597a5e61f5f4c5742b091cbc694
Mojid Eroje <93765911+Eroje@users.noreply.github.com>
2023-02-22 17:50:21 +0000

    Added Calculator.

---
+++ index.js
@@ -0,0 +1,10 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+undefined
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+undefined
+function calculate (num1, num2, operator) {
+    return operator(num1, num2);
